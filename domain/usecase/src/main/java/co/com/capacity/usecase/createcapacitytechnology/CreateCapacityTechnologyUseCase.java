9606eec91fc555d84bd3bebd6adf3e6004411099
Brayan Guerrero <brayanstewartguor@ufps.edu.co>
2026-04-30 13:36:14 -0500

    feat: add capacity-technology use cases

--- domain/usecase/src/main/java/co/com/capacity/usecase/createcapacitytechnology/CreateCapacityTechnologyUseCase.java
+++ domain/usecase/src/main/java/co/com/capacity/usecase/createcapacitytechnology/CreateCapacityTechnologyUseCase.java
@@ -1,6 +1,36 @@
 package co.com.capacity.usecase.createcapacitytechnology;
 
+import co.com.capacity.model.capacity.Capacity;
+import co.com.capacity.model.capacity.gateways.CapacityRepository;
+import co.com.capacity.model.capacitytechnology.CapacityTechnology;
+import co.com.capacity.model.capacitytechnology.CapacityTechnologyEvent;
+import co.com.capacity.model.capacitytechnology.gateways.CapacityTechnologyEventGateway;
+import co.com.capacity.model.capacitytechnology.gateways.CapacityTechnologyRepository;
+import co.com.capacity.model.utils.exception.NotFoundException;
+import co.com.capacity.model.utils.GlobalExceptionEnum;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
-public class CreateCapacityTechnologyUseCase {
+public class CreateCapacityTechnologyUseCase implements CreateCapacityTechnologyService {
+
+    private final CapacityTechnologyRepository capacityTechnologyRepository;
+    private final CapacityTechnologyEventGateway eventGateway;
+    private final CapacityRepository capacityRepository;
+
+    @Override
+    public Mono<CapacityTechnology> create(CapacityTechnology capacityTechnology) {
+        return capacityRepository.findById(capacityTechnology.getCapacityId())
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(GlobalExceptionEnum.CAPACITY_NOT_FOUND)))
+                .flatMap(capacity -> capacityTechnologyRepository.save(capacityTechnology))
+                .flatMap(saved -> {
+                    CapacityTechnologyEvent event = CapacityTechnologyEvent.builder()
+                            .capacityId(saved.getCapacityId())
+                            .technologyCatalogId(saved.getTechnologyId())
+                            .build();
+                    return eventGateway.publishCreated(event)
+                            .thenReturn(saved);
+                });
+    }
 }
--- domain/usecase/src/main/java/co/com/capacity/usecase/getcapacitytechnology/GetCapacityTechnologyUseCase.java
+++ domain/usecase/src/main/java/co/com/capacity/usecase/getcapacitytechnology/GetCapacityTechnologyUseCase.java
@@ -1,6 +1,27 @@
 package co.com.capacity.usecase.getcapacitytechnology;
 
+import co.com.capacity.model.capacitytechnology.CapacityTechnology;
+import co.com.capacity.model.capacitytechnology.gateways.CapacityTechnologyRepository;
+import co.com.capacity.model.utils.exception.NotFoundException;
+import co.com.capacity.model.utils.GlobalExceptionEnum;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
-public class GetCapacityTechnologyUseCase {
+public class GetCapacityTechnologyUseCase implements GetCapacityTechnologyService {
+
+    private final CapacityTechnologyRepository capacityTechnologyRepository;
+
+    @Override
+    public Mono<CapacityTechnology> getById(Long id) {
+        return capacityTechnologyRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(GlobalExceptionEnum.CAPACITY_TECHNOLOGY_NOT_FOUND)));
+    }
+
+    @Override
+    public Flux<CapacityTechnology> findByCapacityId(Long capacityId) {
+        return capacityTechnologyRepository.findByCapacityId(capacityId);
+    }
 }
